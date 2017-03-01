f1106bb2f66331e32db43cb3c70572218581cf13
fengmnegchang <624926379@qq.com>
2017-03-01 16:59:44 +0800

    搜索tab

---
+++ zcool/src/com/open/zcool/activity/ToSearchMainIndicatorFragmentActivity.java
@@ -0,0 +1,67 @@
+/**
+ *****************************************************************************************************************************************************************************
+ * 
+ * @author :fengguangjing
+ * @createTime:2017-2-9下午5:08:07
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ *****************************************************************************************************************************************************************************
+ */
+package com.open.zcool.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+
+import com.open.zcool.R;
+import com.open.zcool.fragment.ToSearchMainIndicatorFragment;
+import com.open.zcool.utils.UrlUtils;
+
+/**
+ *****************************************************************************************************************************************************************************
+ * 网站搜索  Indicator
+ * @author :fengguangjing
+ * @createTime:2017-2-9下午5:08:07
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ *****************************************************************************************************************************************************************************
+ */
+public class ToSearchMainIndicatorFragmentActivity  extends ZcoolCommonFragmentActivity{
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.open.enrz.activity.CommonFragmentActivity#initValue()
+	 */
+	@Override
+	protected void initValue() {
+		// TODO Auto-generated method stub
+		if (getIntent().getStringExtra("URL") != null) {
+			url = getIntent().getStringExtra("URL");
+		} else {
+			url = UrlUtils.ZCOOL_TO_SEARCH;
+		}
+		addfragment();
+	}
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.open.android.activity.CommonFragmentActivity#addfragment()
+	 */
+	@Override
+	public void addfragment() {
+		// TODO Auto-generated method stub
+		Fragment fragment = ToSearchMainIndicatorFragment.newInstance(url, true);
+		getSupportFragmentManager().beginTransaction().replace(R.id.id_common_fragment, fragment).commit();
+	}
+
+	public static void startToSearchMainIndicatorFragmentActivity(Context context, String url) {
+		Intent intent = new Intent();
+		intent.putExtra("URL", url);
+		intent.setClass(context, ToSearchMainIndicatorFragmentActivity.class);
+		context.startActivity(intent);
+	}
+}
---
+++ zcool/src/com/open/zcool/jsoup/ToSearchMainTabService.java
@@ -0,0 +1,72 @@
+package com.open.zcool.jsoup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import android.util.Log;
+
+import com.open.android.jsoup.CommonService;
+import com.open.zcool.bean.DesignerTabBean;
+import com.open.zcool.utils.UrlUtils;
+
+public class ToSearchMainTabService extends CommonService {
+	public static final String TAG = ToSearchMainTabService.class.getSimpleName();
+	
+	public static List<DesignerTabBean> parseDesignerTab(String href) {
+		List<DesignerTabBean> list = new ArrayList<DesignerTabBean>();
+		try {
+			Log.i(TAG, "url = " + href);
+			Document doc = Jsoup.connect(href).userAgent(UrlUtils.enrzAgent).timeout(10000).get();
+			// System.out.println(doc.toString());
+			try {
+				Element globalnavElement = doc.select("div.camNavC").first();
+				Elements moduleElements = globalnavElement.select("a");
+				if (moduleElements != null && moduleElements.size() > 0) {
+					for (int i = 0; i < moduleElements.size(); i++) {
+						DesignerTabBean sbean = new DesignerTabBean();
+						try {
+							/**
+							<div class="camNavC">
+					    	<a href="tosearch.do?page=0&world="  class="selected">全站</a>
+					    	<a href="tosearch.do?page=1&world=" >原创作品</a>
+					    	<a href="tosearch.do?page=2&world=" >设计文章</a>
+					    	<a href="tosearch.do?page=3&world=" >佳作欣赏</a>
+					    	<a href="tosearch.do?page=4&world=" >设计素材</a>
+					    	<a href="tosearch.do?page=5&world=" >设计师</a>
+					    	<a href="http://www.hellorf.com/hellorf/search/all/.html" target="_blank">正版图片</a>
+				    	</div>
+							 */
+							try {
+								Element aElement = moduleElements.get(i).select("a").first();
+								if (aElement != null) {
+									String title = aElement.text();
+									Log.i(TAG, "i==" + i + ";title==" + title);
+									sbean.setHref(href);
+									sbean.setTitle(title);
+								}
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+						list.add(sbean);
+					}
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+}
